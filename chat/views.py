d0207eaee71724e34ca0499cf37f18a76fcec1a8
amireho3ein <amireedoran@gmail.com>
2023-09-30 14:14:04 +0330

    add models for chat room and simple view

--- chat/views.py
+++ chat/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Room
+
+
+def index_view(request):
+
+    context = {
+        'rooms': Room.objects.all()
+    }
+
+    return render(request, 'index.html', context)
+
+
+def room_view(request, room_name):
+    chat_room = Room.objects.get_or_create(name=room_name)
+    context = {
+        'room': chat_room
+    }
+    return render(request, 'room.html', context)
